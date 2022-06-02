2afa1f5692723a8e3a21446d51ed1a09e262b63b
ibrahimhimself <ibrhaimaboshanab1699@gmail.com>
2022-06-03 01:11:45 +0200

    Adding Editing

--- images/Vector Art/a.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import glob, os
-
-for i in glob.glob("*.jpg"):
-    file, ext = os.path.splitext(i)
-    print(file)
-    im = Image.open(i).convert("RGB")
-    im.save(file + ".webp", "WEBP")
